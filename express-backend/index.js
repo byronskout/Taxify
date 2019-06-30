b24c470c44c8b054dbc17c7abae530432fcd3898
github username <vyskoutaris@hotmail.com>
2019-07-01 00:52:52 +0100

    Connected to DB, post req sends info

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,24 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const User = require("./model/User");
 const PORT = 4000;
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoDbConnectionString =
 "mongodb+srv://testUser:test@cluster0-xctlt.mongodb.net/test?retryWrites=true&w=majority";
+
+app.get("/users", (req, res) => {
+  res.send("You fetched a user!");
+});
+
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+  await user.save();
+  res.send("You saved a user!");
+})
 
 mongoose
 .connect(mongoDbConnectionString, {useNewUrlParser: true})
 .then(result => {
   console.log("Connected to MongoDb");
+  app.listen(PORT, () => {
+    console.log("Server is listening on PORT:" + PORT)
+  });
 })
 .catch(err => {
   console.log(err);
 })
-
-app.get("/user", (req, res) => {
-  res.send("You fetched a user!")
-});
-
-app.listen(PORT, () => {
-  console.log("Server is listening on PORT:" + PORT)
-});
